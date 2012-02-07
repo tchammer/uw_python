d5900b5d464eac6e4a0a7ef1ac93415de679ab70
Timothy Chen <timothyechen@yahoo.com>
2012-02-07 15:49:29 -0800

    added echo_www.py server

---
+++ echo_www/echo_www.py
@@ -0,0 +1,50 @@
+"""
+hello_www.py - minimal web server + web application
+"""
+
+import socket 
+import sys
+
+
+page = """
+HTTP/1.0 200 OK
+Content-Type: text/html
+
+<html>
+<body>
+tchammer: %s
+</body>
+</html>
+"""
+
+host = '' 
+port = 8081 # different default port than thirty_minute_webserver
+
+# optional command line argument: port 
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+
+backlog = 5 
+size = 2048 
+
+# server's listener socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# Release listener socket immediately when program exits, 
+# avoid socket.error: [Errno 48] Address already in use
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind((host,port)) 
+
+print 'hello_www listening on port', port
+s.listen(backlog) 
+
+while True: # just keep serving page to any client that connects
+    client, address = s.accept() # create client socket
+    data = client.recv(size)
+
+    lines = data.split('\n')
+    breakup = lines[0].split(" ")
+    client.send(page % breakup[1][1:])
+    
+    client.close()
